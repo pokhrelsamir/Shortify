bb61a5304ea21b6101272c31a886882a547b68bf
Samir Pokhrel <poksamir6@gmail.com>
2026-08-12 22:21:10 +0545

    links/admin.py file Initiation

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShortURL
+
+
+@admin.register(ShortURL)
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = (
+        "short_code",
+        "original_url",
+        "clicks",
+        "created_at",
+    )
+
+    search_fields = (
+        "short_code",
+        "original_url",
+    )
+
+    list_filter = (
+        "created_at",
+    )
+
+    readonly_fields = (
+        "clicks",
+        "created_at",
+        "updated_at",
+    )
