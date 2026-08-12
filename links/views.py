61e4a32dde1e3c3d937ea72c84b97af9d23476d2
Samir Pokhrel <poksamir6@gmail.com>
2026-08-12 22:34:09 +0545

    links/views.py Integration

--- links/views.py
+++ links/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ShortURLForm
+
+
+def home(request):
+    short_url = None
+
+    if request.method == "POST":
+        form = ShortURLForm(request.POST)
+
+        if form.is_valid():
+            link = form.save()
+            short_url = link
+    else:
+        form = ShortURLForm()
+
+    context = {
+        "form": form,
+        "short_url": short_url,
+    }
+
+    return render(
+        request,
+        "links/home.html",
+        context,
+    )
