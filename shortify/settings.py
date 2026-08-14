02fcb5d37286ff1037852c692321a6460ebbfa91
Samir Pokhrel <poksamir6@gmail.com>
2026-08-14 22:06:46 +0545

    Configure production static files

--- shortify/settings.py
+++ shortify/settings.py
@@ -202,6 +202,15 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
 )
